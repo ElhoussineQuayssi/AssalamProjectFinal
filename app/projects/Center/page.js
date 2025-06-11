45e428d963cc3fa83070dbf6866d4480b1d9c009
ElhoussineQuayssi <145578838+ElhoussineQuayssi@users.noreply.github.com>
2025-06-11 18:40:30 +0100

    Remove duplicate import of Link in Center page

--- app/projects/Center/page.js
+++ app/projects/Center/page.js
@@ -1,7 +1,6 @@
 "use client"
 import Image from "next/image"
 import Link from "next/link"
-import Link from "next/link"
 import { CalendarDays, MapPin, Users, Clock, ChevronDown, ChevronUp } from "lucide-react"
 import { useState } from "react"
 
